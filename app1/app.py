dca53ea37e9f91cf5199d2776a819a3fd6a8fd4f
chris <limkai.yuan@zooplus.com>
2020-11-12 21:59:46 +0100

    Update

--- app1/app.py
+++
@@ -1,5 +0,0 @@
-from flask import request, Flask
-import jsonapp1 = Flask(__name__)
-@app1.route('/')def hello_world():
-    return 'Blank Page for App1'if __name__ == '__main__':
-    app1.run(debug=True, host='0.0.0.0')
